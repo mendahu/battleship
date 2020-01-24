a1e064e988135ec5977a44af59fd31dac39a7190
Jake Robins <mendahu@gmail.com>
2020-01-24 21:19:34 +0000

    setup basic templating

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("Battleship Server Running!");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
 app.use(express.static('public'));
 
-// 404
+/*******************************
+GET ROUTING
+*******************************/
+
+app.get("/", (req, res) => {
+  res.render("public/welcome");
+});
+
+
+// 404 CATCH
 app.use(function(req, res, next) {
   res.status(404).send("Path or File not found.");
+});
+
+app.listen(port, () => {
+  console.log("Battleship Server Running!");
 });
