7ab85de5492ac7454e384d7b2a070e8e0e4a8e42
Jake Robins <mendahu@gmail.com>
2020-01-24 21:29:19 +0000

    restructured folder for EJS, installed packages required for server

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
+//Server Config
+const PORT = process.env.PORT || 8080;
+
+//Requires
 const express = require('express');
 const app = express();
-const port = 3000;
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const methodOverride = require('method-override');
+const bodyParser = require("body-parser");
 
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 /*******************************
 GET ROUTING
 *******************************/
 
 app.get("/", (req, res) => {
-  res.render("public/welcome");
+  res.render("welcome");
 });
-
 
 // 404 CATCH
 app.use(function(req, res, next) {
   res.status(404).send("Path or File not found.");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Battleship Server Running!");
 });
